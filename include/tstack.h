f2cbf4154cdeea3a735c52e2a8a113182e355e37
olesiatuliapkina <151723481+olesiatuliapkina@users.noreply.github.com>
2024-04-14 19:35:41 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,48 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T *stackArray;
+    int topIndex;
+
+ public:
+    TStack() {
+        stackArray = new T[size];
+        topIndex = -1;
+    }
+
+    ~TStack() {
+        delete[] stackArray;
+    }
+
+    void push(T item) {
+        if (topIndex != size - 1) {
+            topIndex++;
+            stackArray[topIndex] = item;
+        }
+    }
+
+    T pop() {
+        if (topIndex != -1) {
+            T element = stackArray[topIndex];
+            topIndex--;
+            return element;
+        } else {
+            return T();
+        }
+    }
+
+    T get() const {
+        if (topIndex == -1) {
+            return T();
+        } else {
+            return stackArray[topIndex];
+        }
+    }
+
+    bool isempty() const {
+        return topIndex == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
